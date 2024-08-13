8bf6c47c7c719b4b0b78942f51ac5ede9a79ee1a
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-13 16:08:56 -0400

    Update form with error list

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -19,6 +19,10 @@ class AuthorsController < ApplicationController
 
   def edit
     @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
 
     if @author.update(author_params)
       redirect_to author_path(@author)
